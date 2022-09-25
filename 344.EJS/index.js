44c2124932e77b265ad9d5d8c5ef5aa729178861
Alexis Gordon <alexisgordonn96@gmail.com>
2022-09-25 14:12:47 -0400

    Agregando clase 344 de EJS

--- 344.EJS/index.js
+++ 344.EJS/index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require("path");
 const redditData = require("./data.json");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "/views")); //ver esta clase de nuevo
 
 app.get("/", (req, res) => {
   res.render("home");
